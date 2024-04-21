20afca08a79b9300e668fb90febf8cf2600b3e20
Your Name <you@example.com>
2024-04-21 23:02:14 +0500

    push

--- app/Http/Controllers/Api/VerifyEmailController.php
+++ app/Http/Controllers/Api/VerifyEmailController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Auth\Events\Verified;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class VerifyEmailController extends Controller
 {
